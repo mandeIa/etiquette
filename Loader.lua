a3236c1f7ba17513988b45faa373462f9b84a40b
caiden <163656947+mandeIa@users.noreply.github.com>
2025-05-07 03:23:09 -0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,4 @@
 if not game.IsLoaded(game) then  game.Loaded.Wait(game.Loaded); end
 
+script_key = Etiquette.License
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2b3a50db8059c35dcf263e14f14878fc.lua"))()
-
-script_key = Etiquette.License[1]
